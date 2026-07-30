7d16b63c6e8eb6fe13626d965fc087dda9f4c7a1
BRUNOSR-DEV <bruno77_sr@hotmail.com>
2026-07-30 12:05:01 -0300

    docs: colocando assets no gitignore.

--- avir_af/Scripts/activate.bat
+++ avir_af/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Estudos\Projetos_reais\app_financeiro\avir_af"
+set "VIRTUAL_ENV=C:\-DEV\PROJETOS_REAIS\APP_FINANCEIRO\avir_af"
 
 if not defined PROMPT set PROMPT=$P$G
 
